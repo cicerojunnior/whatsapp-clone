1b8f5cdfa8c6ce3f68b40a2ea6ca5bc0dbac75d5
Cicero Junior <cicerojunior1011@hotmail.com>
2022-09-20 11:18:41 -0300

    configurado estrutura do chat e imports dos emojis

--- src/components/ChatWindow/index.js
+++ src/components/ChatWindow/index.js
@@ -1,12 +1,23 @@
 import React from "react"
-
+import EmojiPicker from "emoji-picker-react"
 import './ChatWindow.css'
 
 import Search from "@mui/icons-material/Search"
 import MoreVert from "@mui/icons-material/MoreVert"
 import AttachFile from "@mui/icons-material/AttachFile" 
+import InsertEmoticon from '@mui/icons-material/EmojiEmotions'
+import CloseIcon from '@mui/icons-material/Close'
+import SendIcon from '@mui/icons-material/Send'
+import MicIcon from '@mui/icons-material/Mic'
+import GifIcon from '@mui/icons-material/GifBox'
+import StickyIcon from '@mui/icons-material/StickyNote2'
 
 export default () => {
+
+    const handleEmojiClick =  () => {
+        
+    }
+
     return (
         <div className="chatWindow">
             <div className="chatWindow--header">
@@ -25,10 +36,47 @@ export default () => {
                 </div>
             </div>
             <div className="chatWindow--body">
-                ...
+                
             </div>
+
+            <div className="chatWindow--emojiArea">
+                <EmojiPicker 
+                    onEmojiClick={handleEmojiClick}
+                    disableSkinTonePicker
+                />
+            </div>
+
             <div className="chatWindow--footer">
-                <AttachFile style={{color: '#919191'}} />
+                <div className="chatWindow--pre">
+                    <div className="chatWindow--btn">
+                        <CloseIcon style={{color: '#919191'}} />
+                    </div>
+                    <div className="chatWindow--btn">
+                        <GifIcon style={{color: '#919191'}} />
+                    </div>
+                    <div className="chatWindow--btn">
+                        <StickyIcon style={{color: '#919191'}} />
+                    </div>
+                    <div className="chatWindow--btn">
+                        <InsertEmoticon style={{color: '#919191'}} />
+                    </div>
+                    <div className="chatWindow--btn">
+                        <AttachFile style={{color: '#919191'}} />
+                    </div>
+                </div>
+                <div className="chatWindow--inputArea">
+                    <input 
+                        ype="text" 
+                        className="chatWindow--input"
+                        placeholder="Mensagem"
+                    />
+                </div>
+                <div className="chatWindow--pos">
+                    <div className="chatWindow--btn">
+                        <SendIcon style={{color: '#919191'}} />
+                    </div>
+                </div>
+                
             </div>
         </div>
     )
